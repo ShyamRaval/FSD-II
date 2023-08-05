deac8ccefad3ec2981e3bea3597932b50706cbc8
Shyam Raval <shyamraval131202@gmail.com>
2023-08-05 13:09:02 +0530

    1:08 pm Saturday, 5 August 2023 (IST) Time in Ahmadabad, Gujarat

---
+++ react/reactfirst/src/form_valid.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+function FormValidation() {
+    const [formdata, setformdata] = useState({})
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+    }
+}
